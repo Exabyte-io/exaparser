0ed3c2d7c94aefe68106219bb9244a1017f7fec7
Mohammad Mohammadi <mohammad@exabyte.io>
2019-02-27 12:15:11 -0800

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -3,16 +3,16 @@ import os
 from src.utils import read_json
 
 # JOB SETTINGS
-
 OWNER = "demo"
 CREATOR = "demo"
 PROJECT = "demo-default"
+JOB_NAME = "External Job"
 
+# Type of Resource Management System (RMS), PBS or SLURM
 RMS_TYPE = "PBS"
 
-DATA_HANDLERS = ["stdout"]
-
-JOB_PREFIX = ""
+# List of data handlers, STDOUT, DISK and EXABYTE
+DATA_HANDLERS = ["EXABYTE"]
 
 # WORKFLOW SETTINGS
 WORKFLOW_TEMPLATE_NAME = "shell.json"
@@ -22,15 +22,16 @@ WORKFLOW_TEMPLATE = read_json(os.path.join(TEMPLATES_DIR, WORKFLOW_TEMPLATE_NAME
 VASP_XML_FILE = "vasprun.xml"
 ESPRESSO_XML_FILE = "data-file.xml"
 
+DISK_DATA_HANDLER_DATA_DIR_NAME = ".exabyte"
+
+# List of properties to extract
 PROPERTIES = [
     "phonon_dos",
-    "convergence_ionic",
     "pressure",
     "atomic_forces",
     "stress_tensor",
     "phonon_dispersions",
     "fermi_energy",
-    "convergence_electronic",
     "density_of_states",
     "zero_point_energy",
     "total_energy_contributions",
