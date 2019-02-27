dacbf0fc00e9dfa4d47f690d182f2c6752121d6d
Mohammad Mohammadi <mohammad@exabyte.io>
2019-02-27 11:13:52 -0800

    Implement exaparser command

--- src/main.py
+++ src/main.py
@@ -5,10 +5,10 @@ from src.job import Job
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-w', '--work-dir', dest="work_dir", help='full path to working directory')
+    parser.add_argument('-w', '--work-dir', dest="work_dir", required=True, help='full path to working directory')
     return parser.parse_args()
 
 
-if __name__ == '__main__':
+def main():
     args = parse_arguments()
     Job(args.work_dir).parse()
