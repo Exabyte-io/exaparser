6928060e87a5f8212ef7f737be61d0738ac5aa58
Mohammad Mohammadi <mohammad@exabyte.io>
2019-03-04 15:38:06 -0800

    docstring

--- tests/integration/test_job.py
+++ tests/integration/test_job.py
@@ -15,7 +15,10 @@ class TestJobParser(IntegrationTestBase):
         super(TestJobParser, self).setUp()
 
     def test_espresso_001_shell_job(self):
-        config = Job(os.path.join(FIXTURES_DIR, "espresso/test-001")).to_json()
+        """
+        Extracts a job from an espresso calculation and asserts the results.
+        """
+        config = Job("External Job", os.path.join(FIXTURES_DIR, "espresso/test-001")).to_json()
         config["workDir"] = ""
         config["workflow"]["subworkflows"][0]["units"][0]["statusTrack"][0]["trackedAt"] = 1551481695
         self.assertDeepAlmostEqual(config, read_json(os.path.join(FIXTURES_DIR, "espresso-001-shell-job.json")))
