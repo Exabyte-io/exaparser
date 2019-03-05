84b345b4e1d7e0db5d57cc5670f66b39feb7f537
Mohammad Mohammadi <mohammad@exabyte.io>
2019-03-05 10:04:05 -0800

    Add VASP test

--- tests/integration/test_job.py
+++ tests/integration/test_job.py
@@ -14,11 +14,22 @@ class TestJobParser(IntegrationTestBase):
     def tearDown(self):
         super(TestJobParser, self).setUp()
 
+    def _clean_job_config(self, config):
+        config["workDir"] = ""
+        config["workflow"]["subworkflows"][0]["units"][0]["statusTrack"][0]["trackedAt"] = 1551481695
+
     def test_espresso_001_shell_job(self):
         """
         Extracts a job from an espresso calculation and asserts the results.
         """
         config = Job("External Job", os.path.join(FIXTURES_DIR, "espresso/test-001")).to_json()
-        config["workDir"] = ""
-        config["workflow"]["subworkflows"][0]["units"][0]["statusTrack"][0]["trackedAt"] = 1551481695
-        self.assertDeepAlmostEqual(config, read_json(os.path.join(FIXTURES_DIR, "espresso-001-shell-job.json")))
+        self._clean_job_config(config)
+        self.assertDeepAlmostEqual(config, read_json(os.path.join(FIXTURES_DIR, "espresso", "shell-job.json")))
+
+    def test_vasp_001_shell_job(self):
+        """
+        Extracts a job from a vasp calculation and asserts the results.
+        """
+        config = Job("External Job", os.path.join(FIXTURES_DIR, "vasp/test-001")).to_json()
+        self._clean_job_config(config)
+        self.assertDeepAlmostEqual(config, read_json(os.path.join(FIXTURES_DIR, "vasp", "shell-job.json")))
