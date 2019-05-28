6eb1107fad083f8483ebf45449f6b3ac2108c7f2
Mohammad Mohammadi <mohammad@exabyte.io>
2019-05-28 22:29:09 +0430

    Add support for QE

--- src/workflow/units/execution/espresso.py
+++ src/workflow/units/execution/espresso.py
@@ -12,3 +12,29 @@ class EspressoExecutionUnit(BaseExecutionUnit):
 
     def __init__(self, config, work_dir):
         super(EspressoExecutionUnit, self).__init__(config, work_dir)
+
+    @property
+    def application(self):
+        """
+        Returns the application used in the unit.
+
+        Returns:
+             dict
+        """
+        return {
+            "name": "espresso",
+            "version": "5.4.0",
+            "summary": "Quantum Espresso"
+        }
+
+    @property
+    def executable(self):
+        """
+        Returns the executable used in the unit.
+
+        Returns:
+             dict
+        """
+        return {
+            "name": "pw.x"
+        }
