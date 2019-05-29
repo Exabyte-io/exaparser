da7f30d05b574f17033cbf7d0988aacf08c078d5
Mohammad Mohammadi <mohammad@exabyte.io>
2019-05-29 14:01:57 +0430

    Update enums.py

--- src/enums.py
+++ src/enums.py
@@ -4,6 +4,13 @@ ESPRESSO_INPUT_FILE_REGEX = r'&CONTROL|&SYSTEM|&ELECTRONS|&IONS|&CELL'
 ESPRESSO_EXECUTABLE_NAME_MAP = {
     "PWSCF": "pw.x",
     "BANDS": "bands.x",
+    "MATDYN": "matdyn.x",
+    "DYNMAT": "dynmat.x",
+    "Q2R": "q2r.x",
+    "PHONON": "ph.x",
+    "PROJWFC": "projwfc.x",
+    "DOS": "dos.x",
+    "NEB": "neb.x",
 }
 
 OUTPUT_CHUNK_SIZE = 1024 * 1024  # in bytes
