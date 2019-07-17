9a21bc360aab44ffacb17ea2853b5819b63e2e54
Mohammad Mohammadi <mohammad@exabyte.io>
2019-07-17 09:21:35 -0700

    Make job multi material

--- src/data/handlers/exabyte.py
+++ src/data/handlers/exabyte.py
@@ -103,7 +103,7 @@ class ExabyteRESTFulAPIDataHandler(DataHandler):
         config["isExternal"] = True
         config["owner"] = self.owner
         config["_project"] = {"_id": self.project["_id"]}
-        if len(materials): config["_material"] = {"_id": materials[0]["_id"]}
+        config["_materials"] = [{"_id": m["_id"]} for m in materials]
         return self.job_endpoints.create(config)
 
     def create_properties(self, job_id):
