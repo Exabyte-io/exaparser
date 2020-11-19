0b86d703812fb6ae6adccbd04c6ebe06141274bd
Simon Adorf <simon.adorf@epfl.ch>
2020-11-19 14:51:04 +0100

    Convert the current main interface to sub-command 'job'.

--- exaparser/cli.py
+++ exaparser/cli.py
@@ -5,13 +5,17 @@ from .data.factory import get_data_handler
 from .job import Job
 
 
-@click.command()
+@click.group()
+def main():
+    pass
+
+
+@main.command()
 @click.option('-n', '--name', required=True, help='job name')
 @click.option('-c', '--config', help='full path to config file')
 @click.option('-w', '--work-dir', required=True, help='full path to working directory')
-def main(name, config, work_dir):
-    if config:
-        ExaParserConfig.read(config)
+def job(name, config, work_dir):
+    if config: ExaParserConfig.read(config)
     job = Job(name, work_dir)
     for handler in ExaParserConfig.get("global", "data_handlers").replace(" ", "").split(","):
         get_data_handler(handler, job).handle()
