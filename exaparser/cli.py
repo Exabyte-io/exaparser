c936e80f88501326a0f55e459d353a9838114497
Simon Adorf <simon.adorf@epfl.ch>
2020-11-19 14:58:01 +0100

    Convert the job -n/--name and -w/--work-dir options to arguments.
    Required options are arguments in disguise.

--- exaparser/cli.py
+++ exaparser/cli.py
@@ -11,10 +11,16 @@ def main():
 
 
 @main.command()
-@click.option('-n', '--name', required=True, help='job name')
+@click.argument('name')
+@click.argument('work-dir', type=click.Path(exists=True, file_okay=False, resolve_path=True))
 @click.option('-c', '--config', help='full path to config file')
-@click.option('-w', '--work-dir', required=True, help='full path to working directory')
-def job(name, config, work_dir):
+def job(name, work_dir, config):
+    """Parse a job.
+
+    Parses the output of job with NAME in directory WORK_DIR. For example:
+
+        ./bin/exaparser job-123 /path/to/workdir/
+    """
     if config: ExaParserConfig.read(config)
     job = Job(name, work_dir)
     for handler in ExaParserConfig.get("global", "data_handlers").replace(" ", "").split(","):
