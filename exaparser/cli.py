87735bed59383422b483b8834eade381ecaa542a
Simon Adorf <simon.adorf@epfl.ch>
2020-11-19 14:46:50 +0100

    Refactor CLI entrypoint to use click instead of argparse.
    Simplifies sub-command interface implementation.

--- exaparser/cli.py
+++ exaparser/cli.py
@@ -1,22 +1,17 @@
-import argparse
+import click
 
 from .config import ExaParserConfig
 from .data.factory import get_data_handler
 from .job import Job
 
 
-def parse_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--name', required=True, help='job name')
-    parser.add_argument('-c', '--config', help='full path to config file')
-    parser.add_argument('-w', '--work-dir', dest="work_dir", required=True, help='full path to working directory')
-    return parser.parse_args()
-
-
-def main():
-    args = parse_arguments()
-    if args.config:
-        ExaParserConfig.read(args.config)
-    job = Job(args.name, args.work_dir)
+@click.command()
+@click.option('-n', '--name', required=True, help='job name')
+@click.option('-c', '--config', help='full path to config file')
+@click.option('-w', '--work-dir', required=True, help='full path to working directory')
+def main(name, config, work_dir):
+    if config:
+        ExaParserConfig.read(config)
+    job = Job(name, work_dir)
     for handler in ExaParserConfig.get("global", "data_handlers").replace(" ", "").split(","):
         get_data_handler(handler, job).handle()
