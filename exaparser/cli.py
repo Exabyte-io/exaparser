ad0c45409669f7a13dba881f35bcd633626f87b3
Simon Adorf <simon.adorf@epfl.ch>
2020-11-19 15:32:17 +0100

    Make '-c/--config' option global.
    This option is equivalent for and independent of sub-commands.

--- exaparser/cli.py
+++ exaparser/cli.py
@@ -6,22 +6,22 @@ from .job import Job
 
 
 @click.group()
-def main():
-    pass
+@click.option('-c', '--config', help='full path to config file', type=click.Path(exists=True))
+def main(config):
+    if config:
+        ExaParserConfig.read(config)
 
 
 @main.command()
 @click.argument('name')
 @click.argument('work-dir', type=click.Path(exists=True, file_okay=False, resolve_path=True))
-@click.option('-c', '--config', help='full path to config file')
-def job(name, work_dir, config):
+def job(name, work_dir):
     """Parse a job.
 
     Parses the output of job with NAME in directory WORK_DIR. For example:
 
         ./bin/exaparser job-123 /path/to/workdir/
     """
-    if config: ExaParserConfig.read(config)
     job = Job(name, work_dir)
     for handler in ExaParserConfig.get("global", "data_handlers").replace(" ", "").split(","):
         get_data_handler(handler, job).handle()
