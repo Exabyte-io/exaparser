2a27ade2d81a85cb4f08548d81fdd313e1baee17
Jonathan Hanson <jonathan@exabyte.io>
2020-11-27 21:16:40 -0800

    More minor cleanup to the integration tests

--- tests/integration/test_job.py
+++ tests/integration/test_job.py
@@ -16,7 +16,7 @@ class TestJobParser(IntegrationTestBase):
 
     def _clean_job_config(self, config):
         # These non-deterministic values in the results need to be overwritten with mock values, so
-        # that our comparisons work.
+        # that our test comparisons work.
         config["workDir"] = "/mock/path"
         config["workflow"]["subworkflows"][0]["units"][0]["statusTrack"][0]["trackedAt"] = 1234567890
 
@@ -24,16 +24,22 @@ class TestJobParser(IntegrationTestBase):
         """
         Extracts a job from an espresso calculation and asserts the results.
         """
-        config = Job("External Job", os.path.join(FIXTURES_DIR, "espresso/test-001")).to_json()
-        self._clean_job_config(config)
         expected = read_json(os.path.join(FIXTURES_DIR, "espresso", "shell-job.json"))
-        self.assertDeepAlmostEqual(expected, config)
+
+        actual = Job("External Job", os.path.join(FIXTURES_DIR, "espresso", "test-001")).to_json()
+        self._clean_job_config(actual)
+
+        self.assertDeepAlmostEqual(expected, actual)
 
     def test_vasp_001_shell_job(self):
         """
         Extracts a job from a vasp calculation and asserts the results.
         """
-        config = Job("External Job", os.path.join(FIXTURES_DIR, "vasp/test-001")).to_json()
-        self._clean_job_config(config)
         expected = read_json(os.path.join(FIXTURES_DIR, "vasp", "shell-job.json"))
-        self.assertDeepAlmostEqual(expected, config)
+
+        actual = Job("External Job", os.path.join(FIXTURES_DIR, "vasp", "test-001")).to_json()
+        self._clean_job_config(actual)
+
+        print actual
+
+        self.assertDeepAlmostEqual(expected, actual)
