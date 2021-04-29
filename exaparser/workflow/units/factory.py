72eedd501b0c748281b4cf3b0df21aaf30e6f36d
Simon Adorf <simon.adorf@epfl.ch>
2021-04-29 17:41:32 +0200

    Minor style improvements.

--- exaparser/workflow/units/factory.py
+++ exaparser/workflow/units/factory.py
@@ -15,7 +15,12 @@ def get_unit(config, work_dir):
         work_dir (str): full path to the job working directory.
     """
     if config["type"] == "execution":
-        execution_units = dict(shell=ShellExecutionUnit, vasp=VaspExecutionUnit, espresso=EspressoExecutionUnit, aiida=AiidaExecutionUnit)
+        execution_units = {
+            "shell": ShellExecutionUnit,
+            "vasp": VaspExecutionUnit,
+            "espresso": EspressoExecutionUnit,
+            "aiida": AiidaExecutionUnit,
+        }
         # Use ShellWithResultsExecutionUnit if parser is asked to extract any results
         if config["application"]["name"] == "shell" and len(config.get("results", [])):
             return ShellWithResultsExecutionUnit(config, work_dir)
